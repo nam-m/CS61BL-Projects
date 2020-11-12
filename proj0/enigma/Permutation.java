56df729f5622d3193feab5b4cad546064b8a8a6a
Nam Anh Mai <maithenamanh@gmail.com>
2020-11-11 17:12:15 -0800

    Fixed permute() and invert() to find character location in cycles instead of alphabet

--- proj0/enigma/Permutation.java
+++ proj0/enigma/Permutation.java
@@ -21,7 +21,7 @@ public class Permutation {
 
     public String[] splitCycles(String str) {
         //Remove left and right parentheses and split permutation cycles into an array
-        return str.replaceAll("[\\(\\)]", "").split("\\s");
+        return str.replaceAll("[()]", "").split("\\s");
     }
 
     /** Return the value of P modulo the size of this permutation. */
@@ -44,7 +44,7 @@ public class Permutation {
     public int permute(int p) {
     	// NOTE: it might be beneficial to have one permute() method always call the other
         // FIXME - How do we use our instance variables to get the index that P permutes to?
-        return _alphabet.toInt(permute(_alphabet.toChar(p)));
+        return _alphabet.toInt(permute(_alphabet.toChar(wrap(p))));
     }
 
     /** Return the index result of applying the inverse of this permutation
@@ -52,7 +52,7 @@ public class Permutation {
     public int invert(int c) {
     	// NOTE: it might be beneficial to have one invert() method always call the other
         // FIXME - How do we use our instance variables to get the index that C inverts to?
-        return _alphabet.toInt(invert(_alphabet.toChar(c)));
+        return _alphabet.toInt(invert(_alphabet.toChar(wrap(c))));
     }
 
     /** Return the character result of applying this permutation to the index
@@ -60,16 +60,18 @@ public class Permutation {
     public char permute(char p) {
     	// NOTE: it might be beneficial to have one permute() method always call the other
         // FIXME - How do we use our instance variables to get the character that P permutes to?
-        int nextCharLocation = _alphabet.toInt(p);
-        String[] cycleString =  splitCycles(_cycles);
-        for (int i=0; i < cycleString.length; i++) {
-            if (cycleString[i].contains("" + p) == true)
-                nextCharLocation = cycleString[i].indexOf(p) + 1;
-                if (nextCharLocation == cycleString[i].length())
+        char nextChar = p;
+        //Split cycles into a string array of permutation groups
+        String[] cycleString = splitCycles(_cycles);
+        for (String s : cycleString) {
+            if (s.contains(String.valueOf(p))) {
+                int nextCharLocation = s.indexOf(p) + 1;
+                if (nextCharLocation == s.length())
                     nextCharLocation = 0;
-                break;
+                nextChar = s.charAt(nextCharLocation);
+            }
         }
-        return _alphabet.toChar(nextCharLocation);
+        return nextChar;
     }
 
     /** Return the character result of applying the inverse of this permutation
@@ -77,16 +79,18 @@ public class Permutation {
     public char invert(char c) {
     	// NOTE: it might be beneficial to have one invert() method always call the other
         // FIXME - How do we use our instance variables to get the character that C inverts to?
-        int prevCharLocation = _alphabet.toInt(c);
-        String[] cycleString =  splitCycles(_cycles);
-        for (int i=0; i < cycleString.length; i++) {
-            if (cycleString[i].contains("" + c) == true)
-                prevCharLocation = cycleString[i].indexOf(c) - 1;
+        char prevChar = c;
+        //Split cycles into a string array of permutation groups
+        String[] cycleString = splitCycles(_cycles);
+        for (String s : cycleString) {
+            if (s.contains(String.valueOf(c))) {
+                int prevCharLocation = s.indexOf(c) - 1;
                 if (prevCharLocation == -1)
-                    prevCharLocation = cycleString.length;
-                break;
+                    prevCharLocation = s.length() - 1;
+                prevChar = s.charAt(prevCharLocation);
+            }
         }
-        return _alphabet.toChar(prevCharLocation);
+        return prevChar;
     }
 
     /** Return the alphabet used to initialize this Permutation. */
@@ -96,6 +100,7 @@ public class Permutation {
 
     /** Alphabet of this permutation. */
     private Alphabet _alphabet;
+    //A string in the form of "(ccc) (cc) ..."
     private String _cycles;
 
     // FIXME - How do we store which letter permutes/inverts to which?
@@ -107,11 +112,11 @@ public class Permutation {
     // javac enigma/Permutation.java enigma/Alphabet.java enigma/CharacterRange.java enigma/EnigmaException.java
     // java enigma/Permutation
     public static void main(String[] args) {
-        Permutation perm = new Permutation("(ABCDEFGHIJKLMNOPQRSTUVWXYZ)", new CharacterRange('A', 'Z'));
+        Permutation perm = new Permutation("(THEV) (CU) (ROS)", new CharacterRange('A', 'Z'));
         System.out.println(perm.size() == 26);
-        System.out.println(perm.permute('Z') == 'A');
-        System.out.println(perm.invert('A') == 'Z');
-        System.out.println(perm.permute(25) == 0);
-        System.out.println(perm.invert(0) == 25);
+        System.out.println(perm.permute('V') == 'T');
+        System.out.println(perm.invert('R') == 'S');
+        System.out.println(perm.permute(3));
+        System.out.println(perm.invert(0));
     }
 }
