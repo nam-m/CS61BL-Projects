156fcf5c37472afbdaadca5ee224c52a8580a784
Nam Anh Mai <maithenamanh@gmail.com>
2020-12-10 23:17:01 -0800

    added atNotch() to detect notch(es) on moving rotor

--- proj0/enigma/MovingRotor.java
+++ proj0/enigma/MovingRotor.java
@@ -3,7 +3,7 @@ package enigma;
 import static enigma.EnigmaException.*;
 
 /** Class that represents a rotating rotor in the enigma machine.
- *  @author
+ *  @author Nam Anh Mai
  */
 public class MovingRotor extends Rotor {
 
@@ -15,6 +15,8 @@ public class MovingRotor extends Rotor {
     public MovingRotor(String name, Permutation perm, String notches) {
         super(name, perm);
         // FIXME - Assign any additional instance variables.
+        _notches = notches;
+        _setting = 0;
     }
 
     // FIXME - This class inherits all of the information present in the
@@ -30,50 +32,65 @@ public class MovingRotor extends Rotor {
 
     @Override // Use this special tag when updating the behavior of a method this class inherits from Rotor
     public boolean rotates() {
-        return false; // FIXME - How do we know whether this Rotor should rotate?
+        // FIXME - How do we know whether this Rotor should rotate?
+        return true;
     }
 
     @Override // Use this special tag when updating the behavior of a method this class inherits from Rotor
     public boolean atNotch() {
-        return false; // FIXME - How do we know whether this Rotor is at a notch?
+        // FIXME - How do we know whether this Rotor is at a notch?
+        // Since there can be more than 1 notch per rotor, notches can be "A"; "DG"; "WRS"
+        String[] notchArray = _notches.split("");
+        for (String s : notchArray) {
+            if (s.equals(String.valueOf(alphabet().toChar(setting())))) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override // Use this special tag when updating the behavior of a method this class inherits from Rotor
     public void advance() {
         // FIXME - What methods can we use to advance this Rotor by one position?
+
     }
 
     // FIXME - How do we keep track of my notches?
+    public String getNotch() {
+        return _notches;
+    }
 
     // FIXME: ADDITIONAL FIELDS HERE, AS NEEDED
+    private String _notches;
+    private int _setting;
 
     // To run this through command line, from the proj0 directory, run the following:
     // javac enigma/Rotor.java enigma/MovingRotor.java enigma/Permutation.java enigma/Alphabet.java enigma/CharacterRange.java enigma/EnigmaException.java
     // java enigma/MovingRotor
     public static void main(String[] args) {
         Permutation perm = new Permutation("(AB) (CDEFGHIJKLMNOPQRSTUVWXYZ)", new CharacterRange('A', 'Z'));
-        MovingRotor rotor = new MovingRotor("forward one", perm, "B");
+        MovingRotor rotor = new MovingRotor("forward one", perm, "CDE");
 
-        System.out.println(rotor.name().equals("forward one"));
-        System.out.println(rotor.alphabet() == perm.alphabet());
-        System.out.println(rotor.permutation() == perm);
-        System.out.println(rotor.rotates() == true);
-        System.out.println(rotor.reflecting() == false);
+//        System.out.println(rotor.name().equals("forward one"));
+//        System.out.println(rotor.alphabet() == perm.alphabet());
+//        System.out.println(rotor.permutation() == perm);
+//        System.out.println(rotor.rotates() == true);
+//        System.out.println(rotor.reflecting() == false);
 
-        System.out.println(rotor.size() == 26);
-        rotor.set(1);
-        System.out.println(rotor.setting() == 1);
-        System.out.println(rotor.atNotch() == true);
-        rotor.set('A');
-        System.out.println(rotor.setting() == 0);
-        System.out.println(rotor.atNotch() == false);
-        System.out.println(rotor.convertForward(0) == 1);
-        System.out.println(rotor.convertBackward(1) == 0);
-        rotor.advance();
-        System.out.println(rotor.setting() == 1);
-        System.out.println(rotor.atNotch() == true);
-        System.out.println(rotor.convertForward(0) == 25);
-        System.out.println(rotor.convertBackward(25) == 0);
+//        System.out.println(rotor.size() == 26);
+        rotor.set(3);
+        System.out.println(rotor.setting());
+        System.out.println(rotor.atNotch());
+        rotor.set('D');
+        System.out.println(rotor.setting());
+        System.out.println(rotor.atNotch());
+//        System.out.println(rotor.convertForward(0) == 1);
+//        System.out.println(rotor.convertBackward(1) == 0);
+//        rotor.advance();
+//        System.out.println(rotor.setting() == 1);
+//        System.out.println(rotor.atNotch() == true);
+//        System.out.println(rotor.convertForward(0) == 25);
+//        System.out.println(rotor.convertBackward(25) == 0);
     }
 
 }
