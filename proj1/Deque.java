7482107d380a60f0441b223bbd89416a5a194caa
Nam Anh Mai <maithenamanh@gmail.com>
2020-12-21 23:55:17 -0800

    added Deque

---
+++ proj1/Deque.java
@@ -0,0 +1,17 @@
+public interface Deque<T> {
+    public void addFirst(T item);
+    public void addLast(T item);
+    /** Returns true if deque is empty, false otherwise. */
+    default boolean isEmpty() {
+        if (size() == 0) {
+            return true;
+        }
+        return false;
+    }
+    public int size();
+    public void printDeque();
+    public T removeFirst();
+    public T removeLast();
+    public T get(int index);
+
+}
