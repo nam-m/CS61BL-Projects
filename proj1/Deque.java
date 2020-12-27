fe43646f1690738538e23eb9f177acafd3a28de3
Nam Anh Mai <maithenamanh@gmail.com>
2020-12-26 23:47:50 -0800

    changed isEmpty() to default

--- proj1/Deque.java
+++ proj1/Deque.java
@@ -2,7 +2,9 @@ public interface Deque<T> {
     public void addFirst(T item);
     public void addLast(T item);
     /** Returns true if deque is empty, false otherwise. */
-    public boolean isEmpty();
+    public default boolean isEmpty() {
+        return (size() == 0);
+    }
     public int size();
     public void printDeque();
     public T removeFirst();
