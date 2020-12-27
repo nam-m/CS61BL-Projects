39370f0e0c1fb555e7aaf94d16f022db80cebb4a
Nam Anh Mai <maithenamanh@gmail.com>
2020-12-26 23:47:09 -0800

    implemented skeleton for ArrayDeque

--- proj1/ArrayDeque.java
+++ proj1/ArrayDeque.java
@@ -1,2 +1,42 @@
-public class ArrayDeque {
+import java.util.ArrayList;
+
+public class ArrayDeque<T> implements Deque<T> {
+
+    public ArrayDeque() {
+
+    }
+    @Override
+    public void addFirst(T item) {
+
+    }
+
+    @Override
+    public void addLast(T item) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void printDeque() {
+
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
 }
