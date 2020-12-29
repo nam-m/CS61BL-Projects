4f91b54ef1d19816a49e62223e904b4fded181cd
Nam Anh Mai <maithenamanh@gmail.com>
2020-12-29 02:29:54 -0800

    Added constructors for ArrayDeque.java

--- proj1/ArrayDeque.java
+++ proj1/ArrayDeque.java
@@ -1,10 +1,24 @@
-import java.util.ArrayList;
+import java.lang.reflect.Array;
 
 public class ArrayDeque<T> implements Deque<T> {
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    // Pointer to array
+    private T ptr;
+    private T[] arr;
 
     public ArrayDeque() {
-
+        // Create a generic type object array and cast it as type T
+        arr = (T[]) new Object [0];
+        ptr = arr[0];
     }
+
+    public ArrayDeque(int size) {
+        arr = (T[]) new Object[size];
+        ptr = arr[0];
+    }
+
     @Override
     public void addFirst(T item) {
 
@@ -17,12 +31,14 @@ public class ArrayDeque<T> implements Deque<T> {
 
     @Override
     public int size() {
-        return 0;
+        return arr.length;
     }
 
     @Override
     public void printDeque() {
-
+        for (T item : arr) {
+            System.out.print(item + " ");
+        }
     }
 
     @Override
@@ -37,6 +53,8 @@ public class ArrayDeque<T> implements Deque<T> {
 
     @Override
     public T get(int index) {
-        return null;
+        if (size <= 0)
+            return null;
+        return arr[index];
     }
 }
