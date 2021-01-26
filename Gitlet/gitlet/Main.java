f5debe145f42bf5474df0bc0c8c539fd53087ae7
Nam Anh Mai <maithenamanh@gmail.com>
2021-01-25 23:54:31 -0800

    nothing

--- Gitlet/gitlet/Main.java
+++ Gitlet/gitlet/Main.java
@@ -1,7 +1,7 @@
 package gitlet;
 
 /* Driver class for Gitlet, the tiny stupid version-control system.
-   @author
+   @author Nam Anh Mai
 */
 public class Main {
 
--- Gitlet/gitlet/UnitTest.java
+++ Gitlet/gitlet/UnitTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /* The suite of all JUnit tests for the gitlet package.
-   @author
+   @author Nam Anh Mai
  */
 public class UnitTest {
 
